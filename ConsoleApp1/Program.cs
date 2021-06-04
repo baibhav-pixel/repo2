752fd3b28131028c8d9523a147f87f32aec07598
baibhav-pixel <baibhav.sinha^@gmail.com>
2021-06-04 15:21:25 +0530

    done changes

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+        public int add(int a,int b)
+        {
+            return a + b;
+        }
+        public int sub(int a,int b)
+        {
+            return a - b;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp1;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Calculator obj = new Calculator();
+        [TestMethod]
+        public void TestAdd()
+        {
+            int act = obj.add(2, 3);
+            int exp = 5;
+            Assert.AreEqual(exp, act);
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            int act = obj.sub(15, 5);
+            int exp = 10;
+            Assert.AreEqual(exp, act);
+        }
+    }
+}
